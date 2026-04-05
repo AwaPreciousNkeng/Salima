6c017b61ee9fca210d42c70fee1bad069dd584f6
AwaPreciousNkeng <awaprecious3t@gmail.com>
2026-04-05 23:41:49 +0100

    Initial commit

---
+++ src/test/java/com/codewithpcodes/salima/SalimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithpcodes.salima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalimaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
