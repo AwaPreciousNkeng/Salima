7c6674b1d3987c27ddc35cd8b85fb87b6495886a
AwaPreciousNkeng <awaprecious3t@gmail.com>
2026-04-06 01:42:57 +0100

    Completed File Management Module

---
+++ src/main/java/com/codewithpcodes/salima/file/FileController.java
@@ -0,0 +1,35 @@
+package com.codewithpcodes.salima.file;
+
+import com.codewithpcodes.salima.user.User;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/files")
+@RequiredArgsConstructor
+@Tag(name = "File Management", description = "File Management Endpoints")
+public class FileController {
+    private final FileService fileService;
+
+    @PostMapping(
+            value = "/upload/profile-picture",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    public ResponseEntity<Map<String, String>> uploadProfilePicture(
+            @RequestParam("file") MultipartFile file,
+            @AuthenticationPrincipal User currentUser
+    ) {
+        String filePath = fileService.saveProfilePicture(file, currentUser.getId());
+        return ResponseEntity.ok(Map.of("filePath", filePath));
+    }
+}
---
+++ src/main/java/com/codewithpcodes/salima/file/FileService.java
@@ -0,0 +1,95 @@
+package com.codewithpcodes.salima.file;
+
+import jakarta.annotation.Nonnull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class FileService {
+
+    @Value("${application.file.uploads.media-output-path}")
+    private String fileUploadPath;
+
+    private static final List<String> ALLOWED_EXTENSIONS = Arrays.asList("jpg", "jpeg", "png", "webp");
+    private static final int MAX_FILE_SIZE = 1024 * 1024 * 2;
+
+    public String saveProfilePicture(
+            @Nonnull MultipartFile sourceFile,
+            @Nonnull UUID userId
+    ) {
+        // Validate
+        validateImageFile(sourceFile);
+
+        //store under users/{userId}/
+        final String subPath = "users" + File.separator + userId;
+        return uploadFile(sourceFile, subPath);
+    }
+
+    private void validateImageFile(MultipartFile sourceFile) {
+        if (sourceFile.isEmpty()) {
+            throw new IllegalArgumentException("File is empty");
+        }
+
+        if (sourceFile.getSize() > MAX_FILE_SIZE) {
+            throw new IllegalArgumentException("File is too large. Max file size is 2MB");
+        }
+
+        String extension = getFileExtension(sourceFile.getOriginalFilename())
+                .replace(".", "");
+
+        if (!ALLOWED_EXTENSIONS.contains(extension)) {
+            throw new IllegalArgumentException("File type is not supported");
+        }
+    }
+
+    private String uploadFile(
+            @Nonnull MultipartFile sourceFile,
+            @Nonnull String fileUploadSubPath
+    ) {
+        final String finalUploadPath = fileUploadPath + File.separator + fileUploadSubPath;
+        File targetFolder = new File(finalUploadPath);
+        if (!targetFolder.exists()) {
+            boolean folderCreated = targetFolder.mkdirs();
+            if (!folderCreated) {
+                log.warn("Failed to create the target folder, {}", targetFolder);
+                throw new RuntimeException("Failed to create upload directory");
+            }
+        }
+        String fileExtension = getFileExtension(sourceFile.getOriginalFilename());
+        String targetFilePath = fileUploadPath + File.separator + System.currentTimeMillis() + fileExtension;
+        Path targetPath = Paths.get(targetFilePath);
+        try {
+            Files.write(targetPath, sourceFile.getBytes());
+            log.info("ProfilePicture saved to {}", targetPath);
+            return targetFilePath;
+        } catch (IOException e) {
+            log.error("File was not saved: {}", e.getMessage());
+            throw new RuntimeException("Failed to save Profile Picture: ", e);
+        }
+    }
+
+    private String getFileExtension(String fileName) {
+        if (fileName == null || fileName.isEmpty()) {
+            return "";
+        }
+        int lastDotIndex = fileName.lastIndexOf('.');
+        if (lastDotIndex == -1) {
+            return "";
+        }
+        return  "." + fileName.substring(lastDotIndex + 1).toLowerCase();
+    }
+}
